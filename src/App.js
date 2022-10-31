560e86102fad464797c5185e4d34fb4610ea0854
Christian <christian.a.faber@gmail.com>
2022-10-31 12:55:21 -1000

    fixed header errors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 import { Board } from "./components/Board";
 import { Sidebar } from "./components/Sidebar";
 import { Top } from "./components/Top";
 import "./index.css";
-// import InitialData from "./components/InitialData";
-// import { Column } from "./components/Column";
 
-function App() {
-  //   state = InitialData;
-  //   return this.state.columnOrder.map((columnID) => {
-  //     const column = this.state.columns[columnID];
-  //     const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
-
-  //     return <Column key={column.id} column={column} tasks={tasks} />;
-  //   });
-  // }
+export const App = () => {
   return (
     <div className="border-solid border-2 flex flex-row">
       <Sidebar />
@@ -24,10 +13,6 @@ function App() {
       </div>
     </div>
   );
-=======
-function App() {
-  return <div>Hello World</div>;
->>>>>>> 96be5219a56ba0d1979615802c1c9e6ce0f8dd07
-}
+};
 
 export default App;
--- src/components/Column.js
+++ src/components/Column.js
@@ -1,26 +1,29 @@
 import { useState } from "react";
-import { Droppable } from "react-beautiful-dnd";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 import InitialData from "./InitialData";
 import { Task } from "./Task";
 
-export const Column = ({{id, title, taskIds} , index}) => {
+export const Column = ({id, tasks, index}) => {
   const [data, setData] = useState(InitialData);
 
   console.log(data);
   console.log(setData);
 
   return (
-    <div>
-      <h2>{data?.columns?.column?.title}</h2>
-      <Droppable droppableId="column">
-        {(provided) => (
-          <ul {...provided.droppableProps} ref={provided.innerRef}>
-            {columns.map({ id, title, taskIds }, index) =>
-            {(<Task key={id} draggableId={id} index={index}/>)}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
-    </div>
+  <div>
+    <h2>{data?.columns?.column?.title}</h2>
+    <Droppable droppableId="column">
+      {(provided) => (
+        <ul {...provided.droppableProps} ref={provided.innerRef}>
+        {columns.map({id, title, taskIds}, index) => {
+          return(
+    <Draggable key={id} draggableId={id} index={index}>
+      {(provided) => (<Task />)}
+    </Draggable>
   );
+}} {provided.placeholder}
+        </ul>
+      )}
+    </Droppable>
+  </div>
 };
