2312c734f8ad9e62f6ea45e193634ecb4c46fd54
Christian <christian.a.faber@gmail.com>
2022-10-31 13:03:59 -1000

    app is up

--- src/components/Column.js
+++ src/components/Column.js
@@ -3,27 +3,30 @@ import { Draggable, Droppable } from "react-beautiful-dnd";
 import InitialData from "./InitialData";
 import { Task } from "./Task";
 
-export const Column = ({id, tasks, index}) => {
+export const Column = ({ id, tasks, index }) => {
   const [data, setData] = useState(InitialData);
 
   console.log(data);
   console.log(setData);
 
   return (
-  <div>
-    <h2>{data?.columns?.column?.title}</h2>
-    <Droppable droppableId="column">
-      {(provided) => (
-        <ul {...provided.droppableProps} ref={provided.innerRef}>
-        {columns.map({id, title, taskIds}, index) => {
+    <div>
+      <h2>{data?.columns?.column?.title}</h2>
+      <Droppable droppableId="column">
+        {(provided) => (
+          <ul {...provided.droppableProps} ref={provided.innerRef}>
+            {/* {columns.map({id, title, taskIds}, index) => {
           return(
-    <Draggable key={id} draggableId={id} index={index}>
-      {(provided) => (<Task />)}
-    </Draggable>
+          <Draggable key={id} draggableId={id} index={index}>
+            {(provided) => (<Task />)}
+          </Draggable> */}
+            {/* );
+}} {provided.placeholder} */}
+
+            <li>test </li>
+          </ul>
+        )}
+      </Droppable>
+    </div>
   );
-}} {provided.placeholder}
-        </ul>
-      )}
-    </Droppable>
-  </div>
 };
