30f6d6bd6f0caf56b2ef49d8baf8a6291944e6b6
Christian <christian.a.faber@gmail.com>
2022-11-08 19:00:37 -1000

    fixed conflict

--- src/components/Top.js
+++ src/components/Top.js
@@ -8,38 +8,44 @@ export const Top = () => {
   const dispatch = useDispatch();
   const [isOpen, setOpen] = useState(false);
   return (
-    <div className="pl-80 flex flex-row text-black ">
-      <div className="flex flex-row ">
-        <button
-          className="position-absolute p-2 rounded-full bg-purple-btn"
-          onClick={() => {
-            dispatch(openTaskModal());
-          }}
-        >
-          +Add New Task
-        </button>
-        <div
-        // container
-        >
-          <div
-            //trigger
-            onClick={() => setOpen(!isOpen)}
-            // className="p-10 absolute h-15 w-15 rounded-lg overflow-hidden cursor-pointer"
+    <div className="flex flex-row">
+      <div className="basis-9/12 flex flex-row">
+        <p className="text-black">Dev-Island Kanban App</p>
+      </div>
+      <div className="">
+        <div className="flex flex-row">
+          <button
+            className="text-white p-2 rounded-full bg-purple-btn"
+            onClick={() => {
+              dispatch(openTaskModal());
+            }}
           >
-            <VerticalEllipsis className="p-10 absolute h-15 w-15 rounded-lg overflow-hidden cursor-pointer" />
-          </div>
-          {isOpen && (
-            <div className="absolute top-25 right-5 rounded-lg p-5 w-50 border border-solid shadow-md">
-              <ul className="py-2.5">
-                <li className="hover:bg-slate-400 rounded-lg p-1">
-                  Edit Boards
-                </li>
-                <li className="hover:bg-slate-400 rounded-lg p-1">
-                  Edit Columns
-                </li>
-              </ul>
+            +Add New Task
+          </button>
+
+          <div
+          // container
+          >
+            <div
+              //trigger
+              onClick={() => setOpen(!isOpen)}
+              // className="p-10 absolute h-15 w-15 rounded-lg overflow-hidden cursor-pointer"
+            >
+              <VerticalEllipsis className="p-10 absolute h-15 w-15 rounded-lg overflow-hidden cursor-pointer" />
             </div>
-          )}
+            {isOpen && (
+              <div className="absolute top-25 right-5 rounded-lg p-5 w-50 border border-solid shadow-md">
+                <ul className="py-2.5">
+                  <li className="hover:bg-slate-400 rounded-lg p-1">
+                    Edit Boards
+                  </li>
+                  <li className="hover:bg-slate-400 rounded-lg p-1">
+                    Edit Columns
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
