c59d5b4817dd8ac4386984dd5c5db0a647476a7c
Christian <christian.a.faber@gmail.com>
2022-11-26 12:49:01 -0600

    Why no work?

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { Board } from "./components/Board";
-
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
 import { Page } from "./Page";
 import { Start } from "./components/Start";
-
+import { store } from "./store/store";
+import { saveState } from "./localStorage";
 export const LightContext = React.createContext();
 export const BoardContext = React.createContext();
 
+store.subscribe(() => {
+  saveState({
+    boards: store.getState().boards,
+    columns: store.getState().columns,
+    tasks: store.getState().tasks,
+  });
+});
+
 export const App = () => {
   const [light, setLight] = useState(true);
-
 
   useEffect(() => {
     if (!light) {
@@ -22,16 +29,14 @@ export const App = () => {
   }, [light]);
 
   return (
-    <LightContext.Provider value={{ light, setLight }}>
+    <LightContext.Provider value={{ light, setLight }} store={store}>
       <div className="font-jakarta overflow-x-auto bg-white dark:bg-gray-800">
-
         <Routes>
           <Route element={<Page />}>
             <Route path="/" element={<Start />} />
             <Route path="/:boardName" element={<Board />} />
           </Route>
         </Routes>
-
       </div>
     </LightContext.Provider>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
 import columnReducer from "../features/columnSlice";
 import taskReducer from "../features/taskSlice";
 import modalReducer from "../features/modalSlice";
 import boardReducer from "../features/boardSlice";
-
 import subtaskReducer from "../features/subtaskSlice";
-
 import sidebarReducer from "../features/sidebarSlice";
+import { loadState } from "../localStorage";
+import { configureStore } from "@reduxjs/toolkit";
 
+const persistedState = loadState();
+const initialStore = {
+  boards: [],
+  columns: [],
+  tasks: [],
+  subtasks: [],
+  persistedState,
+};
 
-export const store = configureStore({
-  reducer: {
-    column: columnReducer,
-    task: taskReducer,
-    modal: modalReducer,
-    board: boardReducer,
-
-    subtask: subtaskReducer,
-
-    sidebar: sidebarReducer,
-
+export const store = configureStore(
+  {
+    reducer: {
+      column: columnReducer,
+      task: taskReducer,
+      modal: modalReducer,
+      board: boardReducer,
+      subtask: subtaskReducer,
+      sidebar: sidebarReducer,
+    },
   },
-});
+  persistedState
+);
 
 //create
